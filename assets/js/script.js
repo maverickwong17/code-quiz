bec481bb0c1545ebb79b0ba47bc75fc87db9161b
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-06-25 22:23:25 -0700

    basic css items complete, some js variables assigned

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+//define elements within the DOM
+var questionhead = document.querySelector("#questionhead");
+var answer = document.querySelectorAll(".answer");
+var a1 = document.querySelector(a1);
+var a2 = document.querySelector(a2);
+var a3 = document.querySelector(a3);
+var a4 = document.querySelector(a4);
+var timer = document.getElementById("#timer")
+
+// questions object to refer back to
+var questions = {
+    q1 : { question : "asdf",
+        option1: "asdf",
+        option2: "asdf",
+        option3: "asdf",
+        option4: "asdf"
+    },
+    q2 : { question : "asdf",
+        option1: "asdf",
+        option2: "asdf",
+        option3: "asdf",
+        option4: "asdf"
+    },
+    q3 : { question : "asdf",
+        option1: "asdf",
+        option2: "asdf",
+        option3: "asdf",
+        option4: "asdf"
+    },
+    q4 : { question : "asdf",
+        option1: "asdf",
+        option2: "asdf",
+        option3: "asdf",
+        option4: "asdf"
+    },
+    q5 : { question : "asdf",
+        option1: "asdf",
+        option2: "asdf",
+        option3: "asdf",
+        option4: "asdf"
+    }
+}
+
+//
